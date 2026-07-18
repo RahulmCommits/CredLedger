8c9cb494330b2b0bf6ad3297d32b5db20fbfc446
rahul mondal <rahul@credledger.com>
2026-07-19 01:24:18 +0530

    fix: set default theme to light and disable system preference override

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} ${greatVibes.variable} antialiased selection:bg-signal-red selection:text-white`}>
         <ErrorBoundary>
           <Providers>
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
               {children}
             </ThemeProvider>
           </Providers>
